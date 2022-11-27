e5b92631610e99362e505cbd37f3bca9342bc956
gp-18 <parthgupta1803@gmail.com>
2022-11-27 17:31:11 +0530

    responsiveness added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// JavaScript
-
-document.getElementById("sign").addEventListener("click", function () {
-  alert("Hello World!");
+let resp = document.getElementById("menubarbtn");
+let slider = document.getElementById("slider");
+let count = 0;
+resp.addEventListener("click",function(){
+  console.log("working")
+  
+  if(count%2 == 0){
+    slider.className += " unresponsive";
+  }
+  else{
+    slider.className = "responsive-navbar"
+  }
+  count++;
 });
